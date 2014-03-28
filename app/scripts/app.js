0d20590b589b25b4df1944a5f91a06913f753c93
John Grubb <therealjohnnygrubb@gmail.com>
2014-03-28 08:44:41 -0400

    init commit

---
+++ app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('archiveApp', [
+  'ngRoute'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
