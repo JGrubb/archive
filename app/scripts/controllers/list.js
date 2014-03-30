04e76f179b0b66bdfb56dd79d99b2c49223c40db
John Grubb <therealjohnnygrubb@gmail.com>
2014-03-30 08:14:21 -0400

    list pages in good shape

--- app/scripts/controllers/list.js
+++ app/scripts/controllers/list.js
@@ -1,30 +1,39 @@
 'use strict';
 
 angular.module('archiveApp')
-  .controller('ListController', function ($scope, Archive, $stateParams, _) {
+.controller('ListController', ['$scope', 'Archive', '$stateParams', '_', 
+  function ($scope, Archive, $stateParams, _) {
 
-    var fullSet;
+  var fullSet;
 
-    Archive.getList($stateParams.collection)
-    .then(function(data) {
-      fullSet = data.response.docs;
+  Archive.getList($stateParams.collection)
+  .then(function(data) {
+    fullSet = data.response.docs;
 
-      var years = _.uniq(_.map(_.pluck(fullSet, 'date'), function(item) {
-        return parseInt(item.slice(0, 4), 10);
-      }), true);
+    var years = _.uniq(_.map(_.pluck(fullSet, 'date'), function(item) {
+      return parseInt(item.slice(0, 4), 10);
+    }), true);
 
-      $scope.shows = fullSet;
-      console.log(fullSet[0]);
-      $scope.years = years;
-      //$scope.yearOf = years.slice(0)[0];
-      $scope.limit = 20;
-    });
-
-    $scope.loadMore = function() {
-      $scope.limit += 20;
-    }
-
-    $scope.setYear = function(year) {
-      $scope.yearOf = year;
-    }
+    $scope.shows = fullSet;
+    console.log(fullSet[0]);
+    $scope.years = years;
+    $scope.years.push('All');
+    //$scope.yearOf = years.slice(0)[0];
+    $scope.limit = 20;
   });
+
+$scope.loadMore = function() {
+  $scope.limit += 20;
+}
+console.log($stateParams);
+
+$scope.yearOf = $stateParams.year;
+
+$scope.setYear = function(year) {
+  if (year === 'All') {
+    $scope.yearOf = undefined;
+  } else {
+    $scope.yearOf = year;
+  }
+}
+} ]);
